330926c4d78f4bcbbe65f4b826b7e8d9ac12e335
Juan Sebastián Cañas <jscanass@gmail.com>
2022-09-02 18:48:52 +0000

    Fix imported packages

--- utils.py
+++ utils.py
@@ -11,12 +11,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 from sklearn.metrics import confusion_matrix
-from maad import sound, util, features
+from maad import sound
 from librosa import feature
 from IPython.display import Audio
 from os import listdir
 from os.path import isfile, join
-
+from tqdm import tqdm
 
 def plot_listen_examples(path_audio,
                          n_fft = 1024,
@@ -214,29 +214,6 @@ def rois_windowed(wl, step, tlims=(0, 60), flims=(0, 22050), rois_annot=None, tn
             #rois.loc[idx_min_t & idx_max_t, 'label'] = row.label
 
     return rois
-
-
-def batch_write_samples(rois, 
-                        wav_path, 
-                        target_sr,
-                        path_save,
-                        flims,
-                        verbose=False):
-    
-    for idx, roi in rois.iterrows():
-        
-        if verbose:
-            print(find_file(roi.fname+'.wav', search_path=wav_path))
-        s, fs = sound.load(find_file(roi.fname, search_path=wav_path))
-    
-        # Preprocessing operations
-        s_trim = sound.trim(s, target_sr, min_t=roi.min_t, max_t=roi.max_t, pad=True)
-        s_resampled = sound.resample(s_trim, fs, target_sr)
-        s_filtered = maad.sound.select_bandwidth(s_resampled,target_sr,fcut=flims, forder=5, fname ='butter', ftype='bandpass')
-        s_normalized = sound.normalize(s_filtered, max_amp=0.7)
-
-        fname_save = os.path.join(path_save, roi.fname_sample)
-        sound.write(fname_save, target_sr, s_normalized, bit_depth=16)
         
 def batch_format_rois(df, wl):
     """ format rois to a fixed length window"""
@@ -249,6 +226,51 @@ def batch_format_rois(df, wl):
     rois_fmt.reset_index(inplace=True, drop=True)
     return rois_fmt
 
+def batch_write_samples(rois, 
+                        wav_path, 
+                        target_sr,
+                        path_save,
+                        flims,
+                        verbose=False):
+    # TODO: Call raw recording 1 time
+    # Multiprocessing
+    for idx, roi in tqdm(rois.iterrows()):
+        s, fs = sound.load(find_file(roi.fname, search_path=wav_path))
+    
+        # Preprocessing operations
+        s_trim = sound.trim(s, target_sr, min_t=roi.min_t, max_t=roi.max_t, pad=True)
+        s_resampled = sound.resample(s_trim, fs, target_sr)
+        s_filtered = sound.select_bandwidth(s_resampled,target_sr,fcut=flims, forder=5, fname ='butter', ftype='bandpass')
+        s_normalized = sound.normalize(s_filtered, max_amp=0.7)
+
+        fname_save = os.path.join(path_save, roi.sample_name)
+        sound.write(fname_save, target_sr, s_normalized, bit_depth=16)
+         
+def batch_write_samples2(rois, 
+                        wav_path, 
+                        target_sr,
+                        path_save,
+                        flims,
+                        verbose=False):
+    
+    # TODO: Multiprocessing or avoid for loops
+    raw_files = rois.fname.unique()
+    for raw_recording in raw_files:
+        if verbose:
+            print(find_file(roi.fname+'.wav', search_path=wav_path))
+        s, fs = sound.load(find_file(raw_recording, search_path=wav_path))
+        # Preprocessing operations
+        s_resampled = sound.resample(s, fs, target_sr)
+        s_filtered = sound.select_bandwidth(s_resampled,target_sr,fcut=flims, forder=5, fname ='butter', ftype='bandpass')
+        s_normalized = sound.normalize(s_filtered, max_amp=0.7)
+        
+        rois_from_file = rois[rois.fname.isin([raw_recording])]
+        for idx, roi in rois_from_file.iterrows():
+            # could we use as last step trim?
+            s_trim = sound.trim(s_normalized, target_sr, min_t=roi.min_t, max_t=roi.max_t, pad=True)
+            fname_save = os.path.join(path_save, roi.sample_name)
+            sound.write(fname_save, target_sr, s_normalized, bit_depth=16)        
+        
 def find_file(filename, search_path):
     """
     File searching tool. Searches a file with filename recurively in a directory.
@@ -269,7 +291,9 @@ def find_file(filename, search_path):
     files_in_path = [f for f in listdir(search_path) if isfile(join(search_path, f))]
     filename_found = [file for file in files_in_path if filename in file]
     if len(filename_found)!=1:
-        warnings.warn("No file found or more than one file has been found")
+        print('file:',filename)
+        print('search_path:',search_path)
+        warnings.warn("No file called found or more than one file has been found")
     return os.path.join(search_path, filename_found[0])
 
     
