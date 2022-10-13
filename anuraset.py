e6061dc191ec7360c01bbe6a5bb9094fdd794c66
Juan Sebastián Cañas <jscanass@gmail.com>
2022-10-13 22:14:45 +0000

    Add transformation functions for each item

--- anuraset.py
+++ anuraset.py
@@ -8,9 +8,12 @@ from torch.utils.data import Dataset
 
 class AnuraSet(Dataset):
     
-    def __init__(self, annotations_file, audio_dir):
+    def __init__(self, annotations_file, audio_dir, transformation,
+                 target_sample_rate):
         self.annotations = pd.read_csv(annotations_file)
         self.audio_dir = audio_dir
+        self.transformation = transformation
+        self.target_sample_rate = target_sample_rate
     
     def __len__(self):
         return len(self.annotations)
@@ -19,7 +22,21 @@ class AnuraSet(Dataset):
         audio_sample_path = self._get_audio_sample_path(index)
         label = self._get_audio_sample_label(index)
         signal, sr = torchaudio.load(audio_sample_path)
+        signal = self._resample_if_necessary(signal, sr)
+        signal = self._mix_down_if_necessary(signal)
+        signal = self.transformation(signal)
         return signal, label
+    
+    def _resample_if_necessary(self, signal, sr):
+        if sr != self.target_sample_rate:
+            resampleer = torchaudio.transforms.Resample(sr, self.target_sample_rate)
+            signal = resampler(signal)
+        return signal
+    
+    def _mix_down_if_necessary(self, signal):
+        if signal.shape[0] > 1:
+            signal = torch.mean(signal, dim=0, keepdim=True)
+        return signal
     
     def _get_audio_sample_path(self, index):
         #fold = f"fold{self.annotations.iloc[index, 10]}"
@@ -34,12 +51,18 @@ class AnuraSet(Dataset):
 if __name__ == "__main__":
     ANNOTATIONS_FILE = "data/BuildDataset/datasetv2-multiclass_1/df_train_test_files.csv"
     AUDIO_DIR = "data/BuildDataset/datasetv2-multiclass_1/audio"
+    SAMPLE_RATE = 22050
     
-    anurasetv2 = AnuraSet(ANNOTATIONS_FILE, AUDIO_DIR)
+    mel_spectrogram = torchaudio.transforms.MelSpectrogram(
+        sample_rate=SAMPLE_RATE,
+        n_fft=1024,
+        hop_length=512,
+        n_mels=64
+    )
+
+    anurasetv2 = AnuraSet(ANNOTATIONS_FILE, AUDIO_DIR, mel_spectrogram,
+                         SAMPLE_RATE)
     print(f"There are {len(anurasetv2)} samples in the dataset.")
     
     signal, label = anurasetv2[0]
-    
-    a = 1
-    
     
