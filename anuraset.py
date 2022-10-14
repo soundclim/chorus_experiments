26681db899f4c841eaffce3604a9400b91de6295
Juan Sebastián Cañas <jscanass@gmail.com>
2022-10-14 00:26:18 +0000

    First CNN model in pytorch

--- anuraset.py
+++ anuraset.py
@@ -1,10 +1,10 @@
 import os
+import torch
 import pandas as pd
 import tqdm as notebook_tqdm
 import torchaudio
 
 from torch.utils.data import Dataset
-
 
 class AnuraSet(Dataset):
     
@@ -13,13 +13,15 @@ class AnuraSet(Dataset):
                  audio_dir, 
                  transformation,
                  target_sample_rate,
-                 num_samples):
+                 num_samples,
+                 device):
         self.annotations = pd.read_csv(annotations_file)
         self.audio_dir = audio_dir
-        self.transformation = transformation
+        self.device = device
+        self.transformation = transformation.to(self.device)
         self.target_sample_rate = target_sample_rate
         self.num_samples = num_samples
-    
+        
     def __len__(self):
         return len(self.annotations)
     
@@ -27,6 +29,7 @@ class AnuraSet(Dataset):
         audio_sample_path = self._get_audio_sample_path(index)
         label = self._get_audio_sample_label(index)
         signal, sr = torchaudio.load(audio_sample_path)
+        signal = signal.to(self.device)
         signal = self._resample_if_necessary(signal, sr)
         signal = self._mix_down_if_necessary(signal)
         signal = self._cut_if_necessary(signal)
@@ -49,7 +52,7 @@ class AnuraSet(Dataset):
     
     def _resample_if_necessary(self, signal, sr):
         if sr != self.target_sample_rate:
-            resampleer = torchaudio.transforms.Resample(sr, self.target_sample_rate)
+            resampleer = torchaudio.transforms.Resample(sr, self.target_sample_rate).cuda()
             signal = resampler(signal)
         return signal
     
@@ -74,18 +77,24 @@ if __name__ == "__main__":
     SAMPLE_RATE = 22050
     NUM_SAMPLES = 66150
     
+    if torch.cuda.is_available():
+        device = "cuda"
+    else:
+        device = "cpu"
+    print(f"Using device {device}")
+    
     mel_spectrogram = torchaudio.transforms.MelSpectrogram(
         sample_rate=SAMPLE_RATE,
         n_fft=1024,
         hop_length=512,
         n_mels=64
     )
-
     anurasetv2 = AnuraSet(ANNOTATIONS_FILE, 
                           AUDIO_DIR, 
                           mel_spectrogram,
-                         SAMPLE_RATE,
-                         NUM_SAMPLES)
+                          SAMPLE_RATE,
+                          NUM_SAMPLES,
+                          device)
     print(f"There are {len(anurasetv2)} samples in the dataset.")
     
     signal, label = anurasetv2[0]
